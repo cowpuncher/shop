7a0434d65c80bc36d2f91cfed3f38d37dea12a3b
WebDev TVlean <tvleanjohnny@gmail.com>
2022-08-30 11:52:41 +0300

    add fetch method

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 
 
 class Product {
-    constructor(image, title, price) {
-        this.image = image;
-        this.title = title;
+    constructor(product_name, price ) {
         this.price = price;
+        this.product_name = product_name;
     }
     render() {
         return `<a href="#" class="goods-item">
                     <div class="goods-img">
                         <img src="${this.image}" alt="Image">
                     </div>
-                    <h3 class="goods-title">${this.title}</h3>
+                    <h3 class="goods-title">${this.product_name}</h3>
                     <p class="goods-price">${this.price}$</p>
                     <button class="goods-add-to-cart">Add to cart</button>
                 </a>`;
@@ -23,70 +22,54 @@ class ProductList {
         this.products = [];
     }
     
-    fetchProducts () {
-        this.products = [
-            { 
-                id: 1, 
-                image: 'https://avatars.mds.yandex.net/get-mpic/5204767/img_id2344845672928735290.jpeg/x248_trim', 
-                title: 'Motobike', 
-                price: 150 
-            },
-            { 
-                id: 2, 
-                image: 'https://avatars.mds.yandex.net/get-mpic/5360356/img_id6907483225986660995.jpeg/x248_trim', 
-                title: 'Monster', 
-                price: 50 
-            },
-            { 
-                id: 3, 
-                image: 'https://avatars.mds.yandex.net/get-mpic/5277279/img_id2084853453747532323.jpeg/x248_trim', 
-                title: 'Mandalorece', 
-                price: 350 
-            },
-            { 
-                id: 4, 
-                image: 'https://avatars.mds.yandex.net/get-mpic/5222168/img_id5521246056186485713.jpeg/x248_trim', 
-                title: 'Mustang', 
-                price: 250 
-            },
-        ];
+    fetchProducts (cb) {
+        const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+        fetch(`${API_URL}/catalogData.json`)
+            .then(response => response.json())
+            .then(json => {
+                this.products = json;
+                cb();
+                let listHtml = '';
+                this.products.forEach(good => {
+                    const goodItem = new Product(good.product_name, good.price);
+                    listHtml += goodItem.render();
+                });
+                document.querySelector('.goods-list').innerHTML = listHtml;
+            })  
+
     }
 
-    static getTotalProductPrice () {
+    getTotalProductPrice () {
         let totalPrice = 0;
         this.products.forEach(product => {
-            totalPrice = totalPrice + product.price
+            totalPrice += product.price;
         });
         document.querySelector('.totalPrice span').innerHTML = totalPrice + '$';
     }
 
-    render() {
-        let listHtml = '';
-        this.products.forEach(product => {
-        const goodItem = new Product(product.image, product.title, product.price);
-        listHtml += goodItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = listHtml;
-    }
 }
 
 const list = new ProductList();
-list.fetchProducts();
-list.render();
-list.getTotalProductPrice();
+list.fetchProducts(() => {
+    list.getTotalProductPrice();
+});
+
 
 class Basket {
     listProductsIn() {
         
     }
-    addTo() {
+    addToBasket() {
 
     }
-    removeFrom() {
+    removeFromBasket() {
 
-    }
+    }    
+}
+class ItemBasket {
     render() {
 
     }
 }
+
 
